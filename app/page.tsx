e72718aa6e044a8ffb715f6c161e1509e735f99b
jaiba147 <felipehiba01@gmail.com>
2024-08-03 03:48:00 -0300

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import AMMInterface from '../components/AmmInterface'
+import Navbar from '../components/navbar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+      <Navbar />
       <AMMInterface />
     </main>
   );
